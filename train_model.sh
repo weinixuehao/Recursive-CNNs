b4dc13533bcf3b153287909cafb40093209b2a38
long.chen <long.chen@ctsmed.com>
2021-11-23 14:35:27 +0800

    Add train_model.sh

---
+++ train_model.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python train_model.py --name NameOfExperiment -i dataset/generated_dataset/selfCollectedData_DocCyclic dataset/generated_dataset/smartdocData_DocTrainC --lr 0.5 --schedule 20 30 35 -v dataset/generated_dataset/smartDocData_DocTestC --batch-size 16 --model-type resnet --loader ram --dataset corner
