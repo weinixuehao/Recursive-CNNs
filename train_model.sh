54230b7f695cf23a18c935a43b95c22ae7888089
long.chen <long.chen@ctsmed.com>
2021-12-10 11:31:10 +0800

    train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-python train_model.py --name DocModel -i dataset/final_dataset/selfCollectedData_DocCyclic dataset/final_dataset/smartdocData_DocTrainC dataset/final_dataset/my_doc_train --lr 0.5 --schedule 20 30 35 -v dataset/final_dataset/smartDocData_DocTestC dataset/final_dataset/my_doc_test --batch-size 16 --model-type resnet --loader ram --epochs 80
+set -e # exit when any command fails
 
-python train_model.py --name CornerModel -i dataset/final_dataset/cornerTrain64 dataset/final_dataset/my_corner_train --lr 0.5 --schedule 20 30 35 -v dataset/final_dataset/selfCollectedData_CornDetec dataset/final_dataset/my_corner_test --batch-size 16 --model-type resnet --loader ram --dataset corner --epochs 80
+if [[ "$1" != "" ]]; then
+    epochs="$1"
+else
+    epochs=40 # The number of epochs is 40.
+fi
+
+echo $epochs
+
+python train_model.py --name DocModel -i dataset/selfCollectedData_DocCyclic dataset/smartdocData_DocTrainC dataset/my_doc_train \
+--lr 0.5 --schedule 20 30 35 -v dataset/smartDocData_DocTestC dataset/my_doc_test --batch-size 16 --model-type resnet --loader ram --epochs $epochs
+
+python train_model.py --name CornerModel -i dataset/cornerTrain64 dataset/my_corner_train \
+--lr 0.5 --schedule 20 30 35 -v dataset/selfCollectedData_CornDetec dataset/my_corner_test --batch-size 16 --model-type resnet --loader ram --dataset corner --epochs $epochs
