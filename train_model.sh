35e4af63e00730ce88c6141194456c4b192f9468
long.chen <long.chen@ctsmed.com>
2021-12-14 14:00:20 +0800

    Add mobile net for training

--- train_model.sh
+++ train_model.sh
@@ -18,8 +18,18 @@ fi
 
 # echo "doc_dataset_train=$doc_dataset_train corner_dataset_test=$corner_dataset_test"
 
+# 1、resnet model
+
+# python train_model.py --name DocModel -i $doc_dataset_train \
+# --lr 0.5 --schedule 20 30 35 -v $doc_dataset_test --batch-size 16 --model-type resnet --loader ram
+
+# python train_model.py --name CornerModel -i $corner_dataset_train \
+# --lr 0.5 --schedule 20 30 35 -v $corner_dataset_test --batch-size 16 --model-type resnet --loader ram --dataset corner
+
+# 2、mobile_net model
+
 python train_model.py --name DocModel -i $doc_dataset_train \
---lr 0.05 --schedule 10 20 30 -v $doc_dataset_test --batch-size 32 --model-type resnet --loader ram
+--lr 0.5 --schedule 20 30 35 -v $doc_dataset_test --batch-size 16 --model-type shallow --loader ram
 
 python train_model.py --name CornerModel -i $corner_dataset_train \
---lr 0.05 --schedule 10 20 30 -v $corner_dataset_test --batch-size 32 --model-type resnet --loader ram --dataset corner
+--lr 0.5 --schedule 20 30 35 -v $corner_dataset_test --batch-size 16 --model-type shallow --loader ram --dataset corner
